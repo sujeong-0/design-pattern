bfb290d561558df5e4dfe0d9d37631619a133766
공수정 <brightelf9@naver.com>
2025-04-17 15:48:48 +0900

    study: 고전적인 싱글톤의 구현 방식

---
+++ src/main/java/com/deepdive/designpattern/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package com.deepdive.designpattern.singleton;
+
+/**
+ * 이 클래스는 싱글톤 패턴의 구현을 담당합니다.
+ */
+public class Singleton {
+
+	private static Singleton instance;
+
+	private Singleton() {
+
+	}
+
+	public static Singleton getInstance() {
+		if (instance == null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+
+	// 이외의 메서드
+}
