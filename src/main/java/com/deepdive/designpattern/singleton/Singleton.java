498b7ed2d1c8448ef89067b62b0c1cf43ab419e7
공수정 <brightelf9@naver.com>
2025-04-17 15:50:32 +0900

    study: 싱글톤의 멀티스레딩 문제 해결방식 해결 방식 1

--- src/main/java/com/deepdive/designpattern/singleton/Singleton.java
+++ src/main/java/com/deepdive/designpattern/singleton/Singleton.java
@@ -11,7 +11,7 @@ public class Singleton {
 
 	}
 
-	public static Singleton getInstance() {
+	public static synchronized Singleton getInstance() {
 		if (instance == null) {
 			instance = new Singleton();
 		}
